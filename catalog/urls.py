c0fae4bbbfa39626e5073a872d644d70d06157bf
Герасимович Никита Юрьевич <herasimovich_mu@peleng.jsc.local>
2022-05-06 14:46:17 +0300

    Добавлены url для новых шаблонов и сделана динамическое создание url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
+
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListViews.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailViews.as_view(), name='book-detail'),
+    re_path(r'authors/$', views.AuthorListViews.as_view(), name='author'),
+    re_path(r'author//(?P<pk>\d+)$', views.AuthorDetailViews.as_view(), name='author-detail'),
 
 ]
